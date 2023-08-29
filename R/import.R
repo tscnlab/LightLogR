2083c3175216341258db815c25140964120c9342
Jzauner <zauner@me.com>
2023-08-29 14:44:05 +0200

    Added a skeleton function to finish the basic setup

---
+++ R/import.R
@@ -0,0 +1,10 @@
+#' Import Dataset from LYS Button
+#'
+#' @param filename Filename for the Dataset. Expects a *character*.
+#'
+#' @return Dataframe with a POSIXct column for the datetime
+#' @export
+
+importLys <- function(filename) {
+  
+}
