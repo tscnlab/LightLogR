61b03e5d60e49553abecef645198e4e83371f598
Johannes Zauner <112665672+JZauner@users.noreply.github.com>
2024-04-09 21:33:19 +0200

    update supported devices

--- data-raw/supported-devices.R
+++ data-raw/supported-devices.R
@@ -1,6 +1,5 @@
 ## code to prepare `supported.devices` dataset goes here
 
-library(LightLogR)
 supported.devices <- names(ll_import_expr) %>% sort()
 
 usethis::use_data(supported.devices, overwrite = TRUE)
