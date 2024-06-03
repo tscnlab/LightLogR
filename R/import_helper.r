1a8f9b48ae4f93dc99916b819b71863acf7cdbc0
Johannes Zauner <112665672+JZauner@users.noreply.github.com>
2024-06-03 15:25:19 +0200

    made more helpful error messages

--- R/import_helper.r
+++ R/import_helper.r
@@ -100,12 +100,20 @@ detect_starting_row <-
   
   #if there is no line with the column names, return an error
   if(length(which_lines) == 0) {
-    stop("Could not find a line with this order of column names in the file. Please check the correct order and spelling of the given columns.")
+    stop("Could not find a line with this order of column names in the file: '",
+         basename(filepath),
+    "'.\n Please check the correct order and spelling of the given columns: '",
+    stringr::str_flatten_comma(column_names), "'")
   }
   
   #if there is more than one line with the column names, return an error
   if(length(which_lines) > 1) {
-    stop(paste("Found", length(which_lines), "lines with the given column names, but require exactly 1. Please provide a more specific pattern."))
+    stop(
+        "Found ", length(which_lines), " lines with the given column names: '", 
+        stringr::str_flatten_comma(column_names),
+        "', but require exactly 1.\n Please provide a more specific pattern or remove the ambiguous lines from the file: '", 
+        basename(filepath), "'"
+      )
   }
   
   #if there is only one line with the column names, return the line number
