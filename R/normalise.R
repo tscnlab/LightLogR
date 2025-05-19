71ca85b6c9848ec7aa293d4a6ec6d9700121ddc4
Johannes Zauner <112665672+JZauner@users.noreply.github.com>
2025-05-19 23:08:18 +0200

    new function

---
+++ R/normalise.R
@@ -0,0 +1,32 @@
+#' Add a defined number to a numeric and log transform it
+#'
+#' Frequently, light exposure data need to be log-transformed. Because light
+#' exposure data frequently also contain many zero-values, adding a small value
+#' avoids losing those observations. Must be applied with care and reported.
+#'
+#' @param x A numeric vector
+#' @param add the amount to add to `x`, by default `0.1`
+#' @param base The logarithmic base, by default `10`
+#'
+#' @returns a transformed numeric vector
+#' @export
+#' 
+#' @references Johannes Zauner, Carolina Guidolin, Manuel Spitschan (2025) How to deal with darkness: Modelling and visualization of zero-inflated personal light exposure data on a logarithmic scale. bioRxiv. doi: https://doi.org/10.1101/2024.12.30.630669
+#'
+#' @examples
+#' c(0, 1, 10, 100, 1000, 10000) |> log_zero_inflated()
+#' 
+#' #For use in a function
+#' sample.data.environment |> 
+#'   dplyr::filter(Id == "Participant") |> 
+#'   dplyr::group_by(Date = lubridate::wday(Datetime, label = TRUE, week_start = 1)) |> 
+#'   dplyr::summarize(
+#'   TAT250 = duration_above_threshold(log_zero_inflated(MEDI), 
+#'                                     Datetime, 
+#'                                     threshold = log_zero_inflated(250)
+#'                                     )
+#'                    )
+log_zero_inflated <- function(x, add = 0.1, base = 10) {
+  if(!is.numeric(x)) stop("x must be numeric")
+  log(x+add, base = base)
+}
