cabf9b7beedde574850fa4ef857759a15c814247
Johannes Zauner <112665672+JZauner@users.noreply.github.com>
2025-05-19 23:06:25 +0200

    new extract function

---
+++ R/extract_metric.R
@@ -0,0 +1,86 @@
+#' Add metrics to extracted sSummary
+#'
+#' This helper function adds metric values to an extract, like from
+#' [extract_states()] or [extract_clusters()]. E.g., the average value of a
+#' variable during a cluster or state instance might be of interest. The metrics
+#' must be specified by the user using the `...` argument.
+#'
+#' The original `data` does not have to have the cluster/state information, but
+#' it will be computationally faster if it does.
+#'
+#' @param extracted_data A dataframe containing cluster or state summaries,
+#'   typically from `extract_clusters()` or `extract_states()`.
+#' @param data The original dataset that produced `extracted_data`
+#' @param identifying.colname Name of the column in `extracted_data` that
+#'   uniquely identifies each row (in addition to the groups. Expects a symbol.
+#'   Defaults to `state.count`
+#' @param Datetime.colname Column name that contains the datetime in `data`.
+#'   Defaults to "Datetime" which is automatically correct for data imported
+#'   with LightLogR. Expects a symbol. This argument is only necessary if `data`
+#'   does not contain the `cluster.colname`.
+#' @param ... Arguments specifying the metrics to add summary. For example:
+#'   `"mean_lux" = mean(lux)`.
+#'
+#' @return A dataframe containing the extracted data with added metrics.
+#'
+#' @export
+#'
+#' @examples
+#' # Extract clusters and add mean MEDI value
+#' sample.data.environment |>
+#' extract_clusters(MEDI > 1000) |>
+#' extract_metric(
+#'   sample.data.environment,
+#'   "mean_lux" = mean(MEDI, na.rm = TRUE)
+#' ) |>
+#' dplyr::select(Id, state.count, duration, mean_lux)
+#'
+#' # Extract states and add mean MEDI value
+#' dataset <-
+#' sample.data.environment |>
+#'  add_photoperiod(c(48.5, 9))
+#'
+#' dataset |>
+#'   extract_states(photoperiod.state) |>
+#'   extract_metric(dataset, mean_lux = mean(MEDI)) |>
+#'   dplyr::select(state.count, duration, mean_lux)
+
+extract_metric <- function(
+    extracted_data,
+    data,
+    identifying.colname = state.count,
+    Datetime.colname = Datetime,
+    
+    ...) {
+  
+  cc.quo <- rlang::enexpr(identifying.colname)
+  cc.name <- rlang::as_name(cc.quo)
+  
+  groups <- dplyr::group_vars(data)
+  
+  #if identifying.colname ist not part of the dataset, it will be added from the extracted_data
+  if (!cc.name %in% names(data)) {
+    data <- 
+      data |>
+      dplyr::left_join(
+        extracted_data |>
+          dplyr::select(-duration, -epoch),
+        by =
+          dplyr::join_by(
+            !!!groups,
+            {{ Datetime.colname }} >= start,
+            {{ Datetime.colname }} <= end
+          )
+      ) |>
+      dplyr::select(-start, -end)
+  }
+  
+  data |>
+    dplyr::filter(!is.na({{ identifying.colname }})) |>
+    dplyr::group_by({{ identifying.colname }}, .add = TRUE) |>
+    dplyr::summarize(
+      ...,
+      .groups = "drop_last"
+    ) |>
+    dplyr::right_join(extracted_data, by =  c(dplyr::group_vars(data), cc.name))
+}
---
+++ R/extract_states.R
@@ -0,0 +1,94 @@
+#' Extract summaries of states
+#'
+#' Extracts a state from a dataset and provides their start and end times, as
+#' well as duration and epoch. The state does not have to exist in the dataset,
+#' but can be dynamically created
+#'
+#' @inheritParams extract_clusters
+#' @param State.colname The variable or condition to be evaluated for state
+#'   exctration. Expects a symbol. If it is not part of the data, a
+#'   `State.expression` is required.
+#' @param State.expression If `State.colname` is not part of the `data`, this
+#'   expression will be evaluated to generate the state. The result of this
+#'   expression will be used for grouping, so it is recommended to be
+#'   factor-like.
+#' @param epoch The epoch to use for the gapless sequence. Can be either a
+#'   `lubridate::duration()` or a string. If it is a string, it needs to be
+#'   either '"dominant.epoch"' (the default) for a guess based on the data or a
+#'   valid `lubridate::duration()` string, e.g., `"1 day"` or `"10 sec"`.
+#'
+#' @returns a dataframe with one row per state instance. Each row will consist
+#'   of the original dataset grouping, the state column. A state.count column,
+#'   start and end Datetimes, as well as a duration of the state
+#' @export
+#'
+#' @examples
+#' #summarizing states "photoperiod"
+#' states <-
+#' sample.data.environment |>
+#'   add_photoperiod(c(48.52, 9.06)) |>
+#'   extract_states(photoperiod.state)
+#' states |> head(2)
+#' states |> tail(2)
+#' states |> summarize_numeric(c(state.count, epoch))
+extract_states <- function(data,
+                           State.colname,
+                           State.expression = NULL,
+                           Datetime.colname = Datetime,
+                           handle.gaps = FALSE,
+                           epoch = "dominant.epoch"
+                           # ignore.FALSE = TRUE
+                           ) {
+  # Convert variable expression to quosure
+  
+  if(colname.defused({{ State.colname }}) %in% names(data)) {
+    State.expression <- rlang::enquo(State.colname)
+  } else State.expression <- rlang::enquo(State.expression)
+  
+  #handle gaps
+  if(handle.gaps) {
+    data <- 
+      data |> gap_handler()
+  }
+  
+  #get the epochs based on the data
+  epochs <- epoch_list(data, Datetime.colname = {{ Datetime.colname }},
+                       epoch = epoch) |> dplyr::pull(dominant.epoch)
+  
+
+  # Calculate lengths of times
+  data <-
+    data |>
+    dplyr::mutate(
+      {{ State.colname }} := !!State.expression,
+      state.count := dplyr::consecutive_id({{ State.colname }}),
+      epoch =  epochs[dplyr::cur_group_id()],
+    ) |>
+    dplyr::group_by({{ State.colname }}, state.count, .add = TRUE)
+  
+  #remove FALSE
+  # state_logical <-
+  #   data |> dplyr::pull({{ State.colname }}) |> is.logical()
+  # 
+  # if(ignore.FALSE & state_logical) {
+  #   data <-
+  #     data |>
+  #     dplyr::mutate({{ State.colname }} := dplyr::na_if({{ State.colname }},0))
+  # }
+  
+  #summarize the instances
+  data |> 
+    dplyr::summarize(
+      epoch = dplyr::first(epoch),
+      start = min({{ Datetime.colname }}) - 0.5*epoch,
+      end = max({{ Datetime.colname }}) + 0.5*epoch,
+      duration = lubridate::as.duration(end - start),
+      .groups = "drop_last"
+    ) |> 
+    dplyr::mutate(state.count = paste({{ State.colname }}, 
+                                      dplyr::consecutive_id(state.count)
+                                      )
+                  )
+  
+}
+  
